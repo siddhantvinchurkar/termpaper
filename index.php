98552459807c7ef9dac96be068d1f13ca981a152
Vinchurkar <siddhantvinchurkar@gmail.com>
2018-03-27 21:31:40 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Siddhant's Term Paper Synopsis</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<section id="header">
+				<header class="major">
+					<h1>Term Paper Synopsis</h1>
+					<p>The less boring version</p>
+				</header>
+				<div class="container">
+					<ul class="actions">
+						<li><a href="#one" class="button special scrolly">Begin</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="main special">
+				<div class="container">
+					<span class="image fit primary"><img src="images/bg1.jpg" alt="" /></span>
+					<div class="content">
+						<header class="major">
+							<h2>What the paper is about</h2>
+						</header>
+						<p>Internet users have grown exponentially in the past decade and as we approach the upper limit of Moore's law, the increased compute power has enabled us to finally employ AI algorithms from the 90's on a large scale. This paper involves performing a study on how chatbots, smart ones backed by AI and the not-so-smart ones based on conditional logic affect the masses.</p>
+					</div>
+					<a href="#two" class="goto-next scrolly">Next</a>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="main special">
+				<div class="container">
+					<span class="image fit primary"><img src="images/bg2.jpg" alt="" /></span>
+					<div class="content">
+						<header class="major">
+							<h2>Things covered in this paper</h2>
+						</header>
+						<p>This paper will be following several user interactions with an artificially intelligent chatbot <a href="https://onyxai.ga/">Onyx</a> with the aim of sorting user behaviour into the following categories&emsp;&rarr;&emsp;</p>
+						<ul class="icons-grid">
+							<li>
+								<span class="icon major fa-user"></span>
+								<h3>User mindset</h3>
+							</li>
+							<li>
+								<span class="icon major fa-comments"></span>
+								<h3>Quality of conversation</h3>
+							</li>
+							<li>
+								<span class="icon major fa-buysellads"></span>
+								<h3>Chatbot appeal</h3>
+							</li>
+							<li>
+								<span class="icon major fa-globe"></span>
+								<h3>Real world applications</h3>
+							</li>
+						</ul>
+					</div>
+					<a href="#three" class="goto-next scrolly">Next</a>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="main special">
+				<div class="container">
+					<span class="image fit primary"><img src="images/bg3.jpg" alt="" /></span>
+					<div class="content">
+						<header class="major">
+							<h2>One more thing</h2>
+						</header>
+						<p>The study may not consider a large dataset of conversations for analysis. Instead, a more targeted experiment will be performed to deduce the required data.</p>
+						<p>Feel free to ask users to talk with <a href="https://facebook.com/onyxai/">Onyx</a>. It will help create a larger dataset to work on.</p>
+						<p>If you're reading this you've probably just skimmed through the entire page without actually reading it. <a href="#one">Click here</a> to back up and read it all. On the other hand if you think I just made a wild accusation and you've actually taken the time to read this,</p>
+<h2>Thank you.</h2>
+					</div>
+
+				</div>
+			</section>
+
+		<!-- Footer -->
+<section id="footer">
+				<footer>
+					<ul class="icons">
+						<li><a href="https://www.facebook.com/sidvinchurkar/" target="_blank" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="https://www.linkedin.com/in/siddhantvinchurkar/" target="_blank" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+						<li><a href="https://github.com/siddhantvinchurkar/" target="_blank" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+						<li><a href="mailto:siddhantvinchurkar@gmail.com" target="_blank" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; <script>document.write(new Date().getFullYear())</script> Volatile, Inc.</li><li>Designed by <a href="https://blog.siddhantvinchurkar.me/" target="_blank">Siddhant Vinchurkar.</a></li>
+					</ul>
+				</footer>
+			</section>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
